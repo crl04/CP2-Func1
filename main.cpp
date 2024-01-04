38472cdd3ae6103deba73046502a9095186e5e05
crl04 <142609376+crl04@users.noreply.github.com>
2024-01-04 12:52:47 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+int Addition();
+int Subtraction();
+int Multiplication();
+int Division(); 
+
+int MainMenu();
+
 int main()
 {
     int add1, add2;
@@ -9,66 +16,27 @@ int main()
     int mul1, mul2;
     int div1, div2;
 
-    int choice;
     char answer;
 
     do {
-    	
-    	system("cls");
-        cout << "Choose among MDAS\n"
-            << "1 ---Addition---\n"
-            << "2 ---Subtraction---\n"
-            << "3 ---Multiplication---\n"
-            << "4 ---Division---\n"
-            << "\n"
-            << "Enter Choice: ";
-        cin >> choice;
-
-        if (choice != 1 && choice != 2 && choice != 3 && choice != 4) {
-            cout << "Invalid input. Please choose a valid option (1, 2, 3, 4)." << endl;
-            
-        }
+        
+        int choice = MainMenu();
 
         switch (choice)
         {
             case 1:
-                cout << "Enter value of 1: ";
-                cin >> add1;
-
-                cout << "Enter another value: ";
-                cin >> add2;
-
-                cout << add1 << " + " << add2 << " = " << add1 + add2;
-                break;
-
+               Addition();
+				break;
             case 2:
-                cout << "Enter value of 1: ";
-                cin >> sub1;
-
-                cout << "Enter another value: ";
-                cin >> sub2;
-
-                cout << sub1 << " - " << sub2 << " = " << sub1 - sub2;
+                Subtraction();
                 break;
 
             case 3:
-                cout << "Enter value of 1: ";
-                cin >> mul1;
-
-                cout << "Enter another value: ";
-                cin >> mul2;
-
-                cout << mul1 << " x " << mul2 << " = " << mul1 * mul2;
+                Multiplication();
                 break;
 
             case 4:
-                cout << "Enter value of 1: ";
-                cin >> div1;
-
-                cout << "Enter another value: ";
-                cin >> div2;
-
-                cout << div1 << " / " << div2 << " = " << div1 / div2;
+                Division();
                 break;
         }
 
@@ -76,7 +44,83 @@ int main()
         cin >> answer;
         system("cls");
 
-    } while (true);
+    } while (answer == 'y' || answer == 'Y');
 
     return 0;
+}
+
+
+int Addition()
+{
+	int add1, add2;
+	
+	add1 + add2;
+	
+
+     cout << add1 << " + " << add2 << " = " << add1 + add2;
+              
+}
+
+int Subtraction()
+{
+	int sub1, sub2;
+	 cout << "Please input value 1 to subtract: ";
+     cin >> sub1;
+
+     cout << "Please input value 2 to subtract: ";
+     cin >> sub2;
+
+     cout << sub1 << " - " << sub2 << " = " << sub1 - sub2;
+}
+
+int Multiplication()
+{
+	int mul1, mul2;
+	cout << "Please input value 1 to Multiply: ";
+    cin >> mul1;
+
+    cout << "Please input value 2 to Multiply: ";
+    cin >> mul2;
+
+    cout << mul1 << " x " << mul2 << " = " << mul1 * mul2;
+}
+
+int Division()
+{
+	int div1, div2;
+	cout << "Please input value 1 to divide: ";
+    cin >> div1;
+
+    cout << "Please input value 2 to divide: ";
+    cin >> div2;
+
+	 if (div2 != 0) {
+        cout << div1 << " / " << div2 << " = " << div1 / div2 << std::endl;
+    }
+    else {
+        cout << "Cannot divide by zero." << std::endl;
+    }
+}
+
+int MainMenu()
+{
+    int choice;
+
+    system("cls");
+    cout << "Choose among MDAS\n"
+        << "1 ---Addition---\n"
+        << "2 ---Subtraction---\n"
+        << "3 ---Multiplication---\n"
+        << "4 ---Division---\n"
+        << "\n"
+        << "Enter Choice: ";
+        
+    cin >> choice;
+
+    if (choice != 1 && choice != 2 && choice != 3 && choice != 4) {
+        cout << "Invalid input. Please choose a valid option (1, 2, 3, 4)." << endl;
+       
+    }
+
+    return choice;
 }
